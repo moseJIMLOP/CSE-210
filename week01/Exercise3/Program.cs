3532c64e9ad3b50d80929c080e3ec3f3ff4de1e7
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2025-05-10 17:57:34 -0600

    program 2

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -5,5 +5,29 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("What is the magic number? "); 
+
+int magicNumber = int.Parse(Console.ReadLine());
+Random randomGenerator = new Random();
+int number = randomGenerator.Next(1, 101);
+
+int guess = -1;
+while (guess != magicNumber)
+{
+    Console.WriteLine("What is your guess? ");
+    guess = int.Parse(Console.ReadLine());
+    if (magicNumber > guess)
+    {
+        Console.WriteLine("Highter");
+    }
+    else if (magicNumber < guess)
+    {
+        Console.WriteLine("Lower");
+    }
+    else
+    {
+        Console.WriteLine(" You guessed it!");
+    }
+}
     }
 }
