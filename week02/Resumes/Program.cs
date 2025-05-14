4787dda1ef2c836ec2b37f907c4c91a4b2de4b18
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2025-05-14 07:24:40 -0600

    I need to complete it

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        public class Job 
+        {
+            public string _company;
+            public string _jobTitle;
+            public int _startYear;
+            public int _endYear;
+
+            public void Display()
+        }    
+        Job Job1 = new Job();
+        Job1._jobTitle = "Software Engineer";
+        
     }
 }
