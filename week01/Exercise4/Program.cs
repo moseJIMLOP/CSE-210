3500dcb8948b6f2cb10a09b1e984e1cb58648e4f
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2025-05-13 22:41:12 -0600

    program

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,43 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int UserNumber = -1;
+        while (UserNumber != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+            string UserResponse = Console.ReadLine();
+            UserNumber = int.Parse(UserResponse);
+
+            if (UserNumber != 0)
+            {
+                numbers.Add(UserNumber);
+            }
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number; 
+            }
+            Console.WriteLine($"The sum is: {sum}");
+
+            float average = ((float)sum) / numbers.Count;
+            Console.WriteLine($"The average is: {average}");
+
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+                Console.WriteLine($"The largest number is: {max}");
+
+            }
+
+
+        }
+
+        
     }
 }
