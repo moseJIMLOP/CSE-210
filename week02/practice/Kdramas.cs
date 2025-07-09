1437e0d1e7fa41503757c4e7aa46066f73d2be7f
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2025-07-09 16:46:05 -0600

    for practice

---
+++ week02/practice/Kdramas.cs
@@ -0,0 +1,6 @@
+public class Kdramas
+{
+    public string _name;
+    public string _about;
+    public int _score;
+}
---
+++ week02/practice/me.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+
+class Me
+{
+    static void Main(string[] args)
+    {
+        Kdramas k1 = new Kdramas();
+        k1._name = "King the Land";
+        k1._about = "A romantic comedy about a chaebol heir and a hotelier.";
+        k1._score = 8;
+
+        Kdramas k2 = new Kdramas();
+        k2._name = "Crash Landing on You";
+        k2._about = "A South Korean heiress accidentally lands in North Korea and meets a North Korean officer.";
+        k2._score = 9;
+
+        Kdramas k3 = new Kdramas();
+        k3._name = "True Beauty";
+        k3._about = "It's about a beautiful man called Suho, wich team are you? team Suho or Team seojun?";
+        k3._score = 10;
+
+        List<Kdramas> KdramasList = new List<Kdramas>();
+        KdramasList.Add(k1);
+        KdramasList.Add(k2);
+        KdramasList.Add(k3);
+
+        foreach (Kdramas k in KdramasList)
+        {
+            Console.WriteLine(k._name);
+        }
+    }
+}
