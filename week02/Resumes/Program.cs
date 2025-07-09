0c1c3149e7d7ef8e6d7f0747bc5663d4b06457f0
moseJIMLOP <mose.jimenez.lopez2002@gmail.com>
2025-07-09 16:08:34 -0600

    w02

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -20,7 +20,7 @@ class Program
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Allison ROSE";
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
 
--- week02/Resumes/resume.cs
+++ week02/Resumes/resume.cs
@@ -6,7 +6,7 @@ public class Resume
     public List<Job> _jobs = new List<Job>();
     public void Display()
     {
-        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine($"Name: {_name}.");
         Console.WriteLine("Jobs:");
         foreach (Job job in _jobs)
         {
